9cd8e17c8384fdb0d788e8d2f7760179402092ca
Jacob Dolph <jacob.dolph702@gmail.com>
2020-05-12 17:04:04 -0600

    removed material ui package

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AppTitle from '../components/AppTitle';
-
+import Card from '../components/Card'
 function Home() {
 
     return (
         <div id="main" class="container-1">
 
             <AppTitle />
+            <Card />
+            <Card />
         </div>
     )
 }
