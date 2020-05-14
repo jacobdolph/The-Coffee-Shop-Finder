bdc26b5771e4031831b058e1cbd073a0b75c57cc
Jacob Dolph <jacob.dolph702@gmail.com>
2020-05-13 20:16:12 -0600

    added a component to house the map

---
+++ client/src/components/Map/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Map() {
+    return (
+        <div className="col s12 m8">
+            <div className="mapColumn" className="col s12" className="flow-flex">
+                <div className="theMap" className="col s12 m8" id="map" style={{ height: "600px" }}>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Map;
